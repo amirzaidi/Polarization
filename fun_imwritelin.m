0892efaaba96793169495fc0510d13d7f7edbf10
Amir Zaidi <azaidi@live.nl>
2022-08-19 18:34:49 +0200

    Additionally output compressed jpeg without brightening

--- fun_imwritelin.m
+++ fun_imwritelin.m
@@ -1,9 +1,9 @@
 function fun_imwritelin(img, file)
     [~, ~] = mkdir('out');
     imwrite(fun_linearrgb_to_srgb(img), strcat('out/', file, '.tif'));
+    imwrite(fun_linearrgb_to_srgb(img), strcat('out/comp_', file, '.jpg'), 'quality', 90);
     
-    [~, ~, dim] = size(img);
-    if (dim > 1)
-        imwrite(fun_linearrgb_to_srgb(img * 2.0), strcat('out/comp_', file, '.jpg'), 'quality', 90);
+    if (size(img, 3) > 1)
+        imwrite(fun_linearrgb_to_srgb(img * 2.0), strcat('out/bright_', file, '.jpg'), 'quality', 90);
     end
 end
